246fd6a4bbbb22e148945596ce127e77d9bf2d6d
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-05-10 16:49:38 +0300

    kintamieji 3pavyzdelis

---
+++ 6-javascript/3-kintamieji/1pvz.js
@@ -0,0 +1,16 @@
+let adresas = 'Gatviu gatvele 10-108, Kaunas';
+let kambariuSkaicius = 3;
+let arYraLiftas = true;
+let arYraKondicionierius = false;
+let kaina = 30000;
+let tipas = 'namas';
+
+console.log(adresas);
+console.log(kambariuSkaicius);
+console.log(arYraLiftas)
+console.log(arYraKondicionierius)
+console.log(kaina);
+console.log(tipas);
+
+
+
---
+++ 6-javascript/3-kintamieji/2pvz.js
@@ -0,0 +1,26 @@
+let automobilis = 'Audi a4';
+let metai = 2012;
+let rida = 300000;
+
+console.log(automobilis);
+console.log(metai);
+console.log(rida);
+
+console.log('Automobilis: '+automobilis);
+console.log('Pagaminimo metai: '+metai);
+console.log('Rida: '+rida+' km');
+
+console.log()
+
+console.log(`Automobilis: ${automobilis}`)
+console.log(`Pagaminimo metai: ${metai}`)
+console.log(`Rida: ${rida} km`)
+
+// Automobilio ____ ridas yra ____ km
+console.log('Automobilio', automobilis, 'rida yra', rida, 'km')
+console.log(`Automobilio ${automobilis} rida yra ${rida} km`)
+
+console.log()
+
+console.log(automobilis, metai, rida);
+console.log('Automobilis', automobilis, 'Pagamintas', metai, 'Rida', rida, 'km')
