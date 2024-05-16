d54c05c23655b241593ed811fa69cc15216cfa63
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-05-16 14:50:16 +0300

    js pasibandymas

--- scripts.js
+++ scripts.js
@@ -51,4 +51,30 @@ console.log(`trecias dar`, antrasParagrafas.nextElementSibling)
 
 /// PADIRBEJIMAS:
 
+let konteineris = document.querySelector(`.container`);
 
+console.log(`pagrindinis div yra:`, konteineris)
+
+let h1 = konteineris.querySelector(`h1`)
+
+console.log(`h1 radau per konteineri:`, h1)
+
+let h2 = konteineris.querySelector(`h1`).nextElementSibling
+
+console.log(`h2 radau per konteineri:`, h2)
+
+let p = konteineris.querySelector(`h1`).nextElementSibling.nextElementSibling
+
+console.log(`p radau per konteineri:`, p)
+
+let p1 = document.querySelector(`.container`).querySelector(`p`)
+
+console.log(`p radau nuo document > per kontaineri ir pirma p`, p1)
+
+let h11 = p1.previousElementSibling.previousElementSibling
+
+console.log(`h1 radau einant nuo p < atgaline tvarka:`, h11)
+
+let konteineris2 = p.parentElement;
+
+console.log(`konteineri ejau griztant nuo vaikinio elemento:`, konteineris2)
