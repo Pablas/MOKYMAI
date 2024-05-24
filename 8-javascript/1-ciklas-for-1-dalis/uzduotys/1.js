95e328766068c549e409cdf23ff89bb4987b2586
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-05-24 09:48:49 +0300

    update uzduociu

--- 8-javascript/1-ciklas-for-1-dalis/uzduotys/1.js
+++ 8-javascript/1-ciklas-for-1-dalis/uzduotys/1.js
@@ -1,7 +1,7 @@
 console.log(`---1:`)
 //1. Parašyti for, kuris išvestų kiekvieną skaičių pradedant nuo 0 ir baigiant 10.
 
-for (let i = 0; i <= 10; i++){
+for (let i = 0; i <= 10; i++) {
     console.log(i)
 }
 
@@ -10,7 +10,7 @@ for (let i = 0; i <= 10; i++){
 console.log(`---2:`)
 //2. Parašyti for, kuris išvestų kas antrą skaičių pradedant 0 ir baigiant 15.
 
-for (let i = 0; i <= 15; i += 2){
+for (let i = 0; i <= 15; i += 2) {
     console.log(i)
 }
 
@@ -19,8 +19,8 @@ for (let i = 0; i <= 15; i += 2){
 console.log(`---3:`)
 //3. Parašyti for, kuris išvestų kas trečią skaičių, pradedant 1 ir baigiant 20.
 
-for (let i = 1; i <= 20; i += 3){
-    console.log(i)
+for (let i = 1; i <= 20; i += 3) {
+    console.log(`[${i}]`)
 }
 
 
@@ -28,8 +28,8 @@ for (let i = 1; i <= 20; i += 3){
 console.log(`---4:`)
 //4. Parašyti for, kuris eitų pro kiekvieną skaičių nuo 1 iki 20. Jame apsirašyti if sąlygą, kuri patikrintų ar dabartinis skaičius dalinasi iš 4, jei taip tai šį skaičių išvesti.
 
-for (let i = 1; i <= 20; i++){
-    if (i % 4 == 0){
+for (let i = 1; i <= 20; i++) {
+    if (i % 4 == 0) {
         console.log(i)
     }
 }
@@ -43,7 +43,7 @@ console.log(`---5:`)
 let start = 32;
 let end = 41;
 
-for (let i = start; i <= end; i++){
+for (let i = start; i <= end; i++) {
     console.log(i, `skaičius kvadratu: ${i * i}`)
 }
 
@@ -55,8 +55,8 @@ console.log(`---6:`)
 let pradzia = 79;
 let pabaiga = 98;
 
-for (let i = pradzia; i <= pabaiga; i++){
-    if (!(i % 2 === 0) || i % 8 == 0){
+for (let i = pradzia; i <= pabaiga; i++) {
+    if (!(i % 2 !== 0) || i % 8 == 0) {
         console.log(i)
     }
 }
@@ -66,18 +66,14 @@ for (let i = pradzia; i <= pabaiga; i++){
 console.log(`---7:`)
 //7. (papildomai, sunkiau) Be daugybos veiksmo programoje, sudauginti du skaičius.
 
-let skaicius1 = 3;
+let skaicius1 = 2;
 let skaicius2 = 5;
-
 
 let suma = 0;
 
-for (let i = 1; i <= 10; i++){
-    if (i % 2 == 0){
-        suma += i;
-    }
-
-    console.log(i)
+for (let i = 0; i < skaicius2; i++) {   
+    suma += skaicius1;
+    console.log(suma)
 }
 
 console.log(`suma:`, suma)
