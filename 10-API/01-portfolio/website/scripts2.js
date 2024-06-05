e293b9db77a0bc3f5068af7aed9804141e6c6dbd
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-05 15:52:18 +0300

    portfolio projektas atvaizduojam naujausia darbo patirti

---
+++ 10-API/01-portfolio/website/scripts2.js
@@ -0,0 +1,17 @@
+fetch(`http://localhost:7788/work-experience/latest/2`)
+    .then(response => response.json())
+    .then(duomenys => {
+        let block = document.querySelector(`.work-experience-list`);
+        
+        for (const duomuo of duomenys) {
+            block.innerHTML += `
+        <div>
+            <h3>${duomuo.company}</h3>
+            <p>${duomuo.title}</p>
+            <p>${duomuo.date}</p>
+            <p>${duomuo.description}</p>
+            <p> Lokacija: ${duomuo.location}</p>
+            <p>${duomuo.skills.map(x => `<span class="badge">${x}</span>`).join(``)}</p>
+        </div>`
+        }
+    })
