aa4448e21951f3d626b6aa42b159cf92662b1f41
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-06 14:10:42 +0300

    tolimesnis kodas

--- 11-react/02-komponentai/src/components/footer/Footer.jsx
+++ 11-react/02-komponentai/src/components/footer/Footer.jsx
@@ -2,7 +2,11 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="container">
+        <p>&copy; Visos teisės saugomos, {new Date().getFullYear()}</p>
+      </div>
+    </footer>
   )
 }
 
--- 11-react/02-komponentai/src/components/navbar/Navbar.jsx
+++ 11-react/02-komponentai/src/components/navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import './Navbar.css';
 
 const Navbar = () => {
-  return (
-    <div>
-        Navbar
-    </div>
-  )
+    return (
+        <nav>
+            <div className="container">
+                <div className="brand">
+                    <a href="#">Projektas</a>
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Link 3</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
