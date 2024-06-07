9ecb254552b6ee9046797d44ca30972dc5dd42e9
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-07 11:00:53 +0300

    react, duomenys, ternary operator

--- 11-react/05-duomenys/src/App.jsx
+++ 11-react/05-duomenys/src/App.jsx
@@ -4,6 +4,7 @@ import Komp2 from './components/Komp2'
 import Komp3 from './components/Komp3'
 import Komp4 from './components/Komp4'
 import Komp5 from './components/Komp5'
+import Komp6 from './components/Komp6'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Komp3/>
             <Komp4/>
             <Komp5/>
+            <Komp6/>
         </>
     )
 }
---
+++ 11-react/05-duomenys/src/components/Komp6.jsx
@@ -0,0 +1,68 @@
+
+
+const Komp6 = () => {
+
+    let pirmas = [`Kaunas`, `Vilnius`, `Klaipeda`]
+    let antras = []
+
+
+    //ternary operator: {klausymas ? taip : ne}
+    //{klausymas && taip} - jei klausymas yra true, tada atvaizduojama taip
+
+    return (
+        <div>
+            <h2>Pirmi miestai:</h2>
+            {
+                pirmas.length > 0 //salyga, ir issivedimas, jeigu yra duomenys ar nera, kas spausdinama:
+                    ?
+                    <p>Duomenu yra</p>
+                    :
+                    <p>Duomenu nera</p>
+            }
+
+            <h2>Pirmi miestai 2</h2>
+
+            {
+                pirmas.length > 0 // taip pat kaip anam pvz, tik jeigu yra duomenu, mes ir spausdiname su map (paimdami kiekviena duomeni).
+                    ?
+                    <ul>
+                        {
+                            pirmas.map((miestas, index) => (
+                                <li key={index}>{miestas}</li>
+                            ))
+                        }
+                    </ul>
+                    :
+                    <p>Duomenu nera</p> // ojei nera, ismeta, kad nera
+            }
+
+            <h2>Antri miestai:</h2>
+
+            {
+                antras.length > 0
+                    ?
+                    <p>Duomenu yra</p>
+                    :
+                    <p>Duomenu nera</p>
+            }
+
+            <h2>Antri miestai 2:</h2>
+
+            {
+                antras.length > 0
+                    ?
+                    <ul>
+                        {
+                            antras.map((x, i) => (
+                                <li key={i}>{x}</li>
+                            ))
+                        }
+                    </ul>
+                    :
+                    <p>Duomenu nera</p>
+            }
+        </div>
+    )
+}
+
+export default Komp6
