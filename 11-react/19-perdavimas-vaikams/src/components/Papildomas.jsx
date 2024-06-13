3be85f38240895bff9cf8276e80d4021133a97d6
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-13 10:40:12 +0300

    react perdavimas tevams susikurimas darbo failu

--- 11-react/19-perdavimas-vaikams/src/components/Papildomas.jsx
+++ 11-react/19-perdavimas-vaikams/src/components/Papildomas.jsx
@@ -1,56 +1,85 @@
-import { useState, useRef } from "react"
+import { useState, useEffect } from "react";
 
 const Papildomas = () => {
-
     const [gyvunai, setGyvunai] = useState([
-        { id: 1, vardas: `Katinas`, amzius: 2},
-        { id: 2, vardas: `Suo`, amzius: 3},
-        { id: 3, vardas: `Paukstis`, amzius: 2},
-    ])
+        { id: 1, vardas: 'Katinas', amzius: 2 },
+        { id: 2, vardas: 'Šuo', amzius: 3 },
+        { id: 3, vardas: 'Paukštis', amzius: 2 },
+    ]);
+
+    const [jauniGyvunai, setJauniGyvunai] = useState([]);
+    const [kiekJaunuGyvunu, setKiekJaunuGyvunu] = useState(gyvunai.filter(gyvunas => gyvunas.amzius < 3).length);
 
     const formaHandler = (event) => {
         event.preventDefault();
-
+        // console.log(event);
+        // console.log(event.target.elements.vardasInput.value);
         let vardas = event.target.elements.vardasInput.value;
-        let amzius = event.target.elements.amziusInput.value;
-        
+        let amzius = event.target.elements.amziusInput.valueAsNumber;
+        // console.log(vardas, amzius);
         let naujasGyvunas = {
             id: Date.now(),
-            vardas: vardas,
-            amzius: amzius
+            vardas: vardas, // tas pats kas: vardas
+            amzius: amzius // tas pats kas: amzius
         }
+        setGyvunai([...gyvunai, naujasGyvunas]);
 
-        setGyvunai([...gyvunai, naujasGyvunas])
+        event.target.elements.vardasInput.value = '';
+        event.target.elements.amziusInput.value = '';
+
+        // setJauniGyvunai(gyvunai.filter(gyvunas => gyvunas.amzius < 3));
+        // console.log(jauniGyvunai); // neveikia, nes atsilieka
+
+        // setKazkas( prevData => [...prevData, naujaInfo] )
+        // pvz:
+        // setGyvunai([...gyvunai, naujasGyvunas]);
+        // setGyvunai(prevGyvunai => [...prevGyvunai, naujasGyvunas2]);
+
+        if (amzius < 3) {
+            setKiekJaunuGyvunu(kiekJaunuGyvunu + 1);
+        }
     }
+
+    useEffect(() => {
+        setJauniGyvunai(gyvunai.filter(gyvunas => gyvunas.amzius < 3));
+    }, [gyvunai])
 
     return (
         <div className="blokas">
-            <h1>PAPILDOMAS PVZ:</h1>
-
+            <h2>Papildomas pavyzdys</h2>
             <form onSubmit={formaHandler}>
                 <div>
                     <label htmlFor="vardasInput">Vardas:</label>
-                    <input type="text" id="vardasInput"/>
+                    <input type="text" id="vardasInput" />
                 </div>
                 <div>
-                    <label htmlFor="amziusInput">Amzius:</label>
-                    <input type="text" id="amziusInput"/>
+                    <label htmlFor="amziusInput">Amzius</label>
+                    <input type="number" id="amziusInput" />
                 </div>
                 <button type="submit">Sukurti</button>
             </form>
-
             {
-                gyvunai.length &&
+                gyvunai.length > 0 &&
                 <ul>
-                    {gyvunai.map(gyv => (
-                        <li key={gyv.id}>
-                            {gyv.vardas} - {gyv.amzius}
+                    {gyvunai.map(gyvunas => (
+                        <li key={gyvunas.id}>
+                            {gyvunas.vardas} - {gyvunas.amzius}
                         </li>
                     ))}
                 </ul>
             }
-
-
+            {
+                gyvunai.filter(gyvunas => gyvunas.amzius < 3) &&
+                <p>Jaunu gyvunu yra: { gyvunai.filter(gyvunas => gyvunas.amzius < 3).length }</p>
+            }
+            {
+                kiekJaunuGyvunu > 0 &&
+                <p>Jaunu gyvunu yra: { kiekJaunuGyvunu }</p>
+            }
+            {
+                jauniGyvunai.length > 0 &&
+                <p>Jaunu gyvunu yra: {jauniGyvunai.length}</p>
+            }
         </div>
     )
 }
