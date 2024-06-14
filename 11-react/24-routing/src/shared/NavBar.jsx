7f7030cf43b38e8886a2778be304e058e565a76d
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-14 14:23:44 +0300

    react routing pavyzdys pabaigtas

--- 11-react/24-routing/src/shared/NavBar.jsx
+++ 11-react/24-routing/src/shared/NavBar.jsx
@@ -1,8 +1,29 @@
-
+import styles from './NavBar.module.css'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className={styles.nav}>
+        <div className={`container ${styles.nav_container}`}>
+            <div className={styles.brand}>
+                <NavLink to="/">Projekto pavadinimas</NavLink>
+            </div>
+            <ul className={styles.links}>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/services">Services</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about-us">About us</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/news">News</NavLink>
+                </li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
