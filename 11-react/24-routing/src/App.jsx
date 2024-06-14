ca80421b8977be6d1a8c09ad35af4e14acf3c9b8
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-14 13:35:58 +0300

    react routing, pridetas about us puslapis

--- 11-react/24-routing/src/App.jsx
+++ 11-react/24-routing/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
+import AboutUsPage from './pages/AboutUsPage'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<MainLayout />}>
                     <Route path='/' element={<HomePage />} />
+                    <Route path='/about-us' element={<AboutUsPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
