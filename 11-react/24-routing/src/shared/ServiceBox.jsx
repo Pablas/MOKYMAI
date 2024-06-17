194f6f78ece64d58074397eadbc19bfe07a41fe3
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-17 12:00:10 +0300

    crud uzklausos

--- 11-react/24-routing/src/shared/ServiceBox.jsx
+++ 11-react/24-routing/src/shared/ServiceBox.jsx
@@ -1,10 +1,23 @@
+import propTypes from 'prop-types'
 const ServiceBox = ({ title, description}) => {
+
+  let stiliai = {
+    background: `#f8f8f8`,
+    padding: `20px`,
+    boxSizing: `border-box`,
+  }
+
   return (
-    <div>
+    <div style={stiliai}>
         <h3>{title}</h3>
         <p>{description}</p>
     </div>
   )
+}
+
+ServiceBox.propTypes = {
+  title: propTypes.string.isRequired,
+  description: propTypes.string.isRequired,
 }
 
 export default ServiceBox
