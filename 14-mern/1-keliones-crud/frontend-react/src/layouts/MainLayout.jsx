09814a9d024edf94b6002af6e7133e045d33a86a
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-19 10:16:19 +0300

    mern keliones, sukurta aplikacija vartotojui

--- 14-mern/1-keliones-crud/frontend-react/src/layouts/MainLayout.jsx
+++ 14-mern/1-keliones-crud/frontend-react/src/layouts/MainLayout.jsx
@@ -1,12 +1,27 @@
 import { Outlet } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import styles from './MainLayout.module.css'
 
 const MainLayout = () => {
-  return (
-    <div>
-        mainpage
-        <Outlet />
-    </div>
-  )
+    return (
+        <div>
+            <nav className={styles.nav}>
+                <div>
+                    <Link to="/">Keliones</Link>
+                </div>
+                <ul className={styles.links}>
+                    <li><NavLink to="/">Pradzia</NavLink></li>
+                    <li><NavLink to="/sukurti">Sukurti nauja</NavLink></li>
+                </ul>
+            </nav>
+            <div className={styles.content}>
+                <Outlet />
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default MainLayout
