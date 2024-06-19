8ad42cc3a3e3fa00a7643aae4452d41d69216059
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-19 13:53:15 +0300

    ikelta info is serverio i react front end

--- 14-mern/1-keliones-crud/frontend-react/src/pages/HomePage.jsx
+++ 14-mern/1-keliones-crud/frontend-react/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
+import styles from './HomePage.module.css'
 //uzklausa i backend (kad gauti info){ useEffect }:
 import { useEffect, useState } from "react"
+
 
 const HomePage = () => {
 
@@ -10,6 +12,7 @@ const HomePage = () => {
         fetch('http://localhost:8675/travels')
             .then(response => response.json()) //i json, javascripta mano info persirase
             .then(data => { //data ir yra masyvas su duomenimis (galima uzvadinti kaip norima)
+                // console.log(data) - pasitikrinti pries, kokius gaunam duomenis
                 setTravels(data) //isistatom duomenis i travels, masyva, bet rasyti viduje po visko, nes turim palaukti kol gaus visa informacija.
             })
     }, [])
@@ -17,10 +20,10 @@ const HomePage = () => {
     return (
         <div>
             <h1>Keliones</h1>
-            <div>
+            <div className={styles.travels_list}>
                 {
                     travels.map(travel => (
-                        <div key={travel._id}>
+                        <div key={travel._id} className={styles.travel_box}>
                             <h3>{travel.title}</h3>
                             <p>{travel.description}</p>
                             <p><strong>Metai:</strong> {travel.year}</p>
