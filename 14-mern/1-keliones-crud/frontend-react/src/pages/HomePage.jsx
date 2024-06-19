6d84b3548e004010776a91a8f6106076b0bd97b7
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-19 10:54:24 +0300

    mern keliones, kelioniu atvaizdavimas ir mongo duomenys ivesti

--- 14-mern/1-keliones-crud/frontend-react/src/pages/HomePage.jsx
+++ 14-mern/1-keliones-crud/frontend-react/src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
 const HomePage = () => {
   return (
     <div>
-        homepage
+        <h1>Keliones</h1>
+        <div>
+            <div>
+                <h3>Portugalija - Lisabona ir Sintra</h3>
+                <p>Kelione po ...</p>
+                <p><strong>Metai:</strong> 2023</p>
+                <p><strong>Kiek naktu:</strong> 9</p>
+                <p><strong>Kaina:</strong> 1000 euru</p>
+            </div>
+        </div>
     </div>
   )
 }
