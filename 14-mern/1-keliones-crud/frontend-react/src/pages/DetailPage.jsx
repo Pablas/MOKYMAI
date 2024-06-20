5b24630f8514384804d1220442957eaad17f00d1
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-20 12:02:01 +0300

    istrinimas ir issitrynus reddirectina i pagrindini puslapi

--- 14-mern/1-keliones-crud/frontend-react/src/pages/DetailPage.jsx
+++ 14-mern/1-keliones-crud/frontend-react/src/pages/DetailPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react" // kad useEffect veiktu ir useState
+import { useNavigate } from "react-router-dom" // kad galetume nureroutinti i kita vieta
 import { useParams } from "react-router-dom" //kad pagal id galetu ieiti i sita puslapi, ima pavadinimo gala (ne failo id), LINKO GALA IMA!!! (is Route /:id - sitas naudojamas cia)
 
 ////////////////////////////////////
@@ -8,6 +9,7 @@ import { useParams } from "react-router-dom" //kad pagal id galetu ieiti i sita 
 
 const DetailPage = () => {
 
+    const navigate = useNavigate() // kad reroutintume zmogu i kita page
     const { id } = useParams()
 
     const [travel, setTravel] = useState({}) //kintamasis kuriame saugoti info kuria gausim is serverio
@@ -21,6 +23,25 @@ const DetailPage = () => {
     }, [id])
 
 
+
+    //mygtukas kuris deletins irasa:
+    const deleteHandler = () => {
+        //duos lentele ar trynti, ir jei true issives, jei spaus no, tada nesuveiks if'as:
+        if (confirm(`Ar tikrai norite trinti "${travel?.title}"?`)) {
+            //env - kur yra nuoroda, id - gaunam is serverio info (puslapio adreso galas):
+            //BACKEND reaguos i sita nurodo kaip route, ir reaguos:
+            //linkas ir metodas ka darys:
+            fetch(`${import.meta.env.VITE_BACKEND_URL}/travels/${id}`, { method: `DELETE`})
+                .then(response => response.json()) //responsa i .json konvertuojam (serverio kalba)
+
+                //po trinimo koks veiksmas bus (cia nukreipiam i "/" page):
+                //reikia import ir const funkcija nusistatyt!!!
+                .then(() => navigate(`/`)) 
+
+        }
+    }
+
+
     return ( // ? - jeigu yra tas duomenis, o jei ne neisves nieko (saugiau, nemes klaidu jeigu nebus)
         <div>
             <h1>Kelione: {travel?.title}</h1>
@@ -28,6 +49,9 @@ const DetailPage = () => {
             <p>Metai: {travel?.year}</p>
             <p>Naktys: {travel?.nights}</p>
             <p>Kaina: {travel?.price} eur.</p>
+            <p>
+                <button onClick={deleteHandler} className="btn btn-red">Trinti</button>
+            </p>
         </div>
     )
 }
