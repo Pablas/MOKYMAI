f9e024cd02d364369bb415fad22371b1b6dd3241
Povilas Rackauskas <rackauskas.p@gmail.com>
2024-06-20 10:28:00 +0300

    useParams sudeti, kad pagal unikalu id butu pasirinktas page

--- 14-mern/1-keliones-crud/frontend-react/src/pages/DetailPage.jsx
+++ 14-mern/1-keliones-crud/frontend-react/src/pages/DetailPage.jsx
@@ -1,7 +1,15 @@
+import { useParams } from "react-router-dom" //kad pagal id galetu ieiti i sita puslapi, ima pavadinimo gala (ne failo id), LINKO GALA IMA!!! (is Route /:id - sitas naudojamas cia)
+
+
 const DetailPage = () => {
-  return (
-    <div>DetailPage</div>
-  )
+
+    const { id } = useParams()
+
+    return (
+        <div>
+            <h1>Kelione: { id }</h1>
+        </div>
+    )
 }
 
 export default DetailPage
